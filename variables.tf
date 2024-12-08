5a4c7350872b8c2731aad9639532cf95c829119c
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 14:03:46 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr_01" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "CIDR block of vpc"
+}
+variable "pub1_cidr_01" {
+  type        = string
+  default     = "10.0.1.0/24"
+  description = "CIDR block of vpc"
+}
+variable "pub2_cidr_01" {
+  type        = string
+  default     = "10.0.2.0/24"
+  description = "CIDR block of vpc"
+}
+variable "pvt1_cidr_01" {
+  type        = string
+  default     = "10.0.3.0/24"
+  description = "CIDR block of vpc"
+}
+variable "pvt2_cidr_01" {
+  type        = string
+  default     = "10.0.4.0/24"
+  description = "CIDR block of vpc"
+}
