03c54f5f9292c600520d1d79d0bdb565eff8c017
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 19:53:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-module "Networking_module" {
-  source    = "./Networking_module"
-  vpc_cidr  = var.vpc_cidr_01
-  pub1_cidr = var.pub1_cidr_01
-  pub2_cidr = var.pub2_cidr_01
-  pvt1_cidr = var.pvt1_cidr_01
-  pvt2_cidr = var.pvt2_cidr_01
-}
-module "Security_module" {
-  source      = "./Security_module"
-  sg_port_pvt = var.sg_port_pvt_01
-  vpc_id_n    = module.Networking_module.vpc_id
-}
+
 
 module "Networking_module" {
   source    = "./Networking_module"
