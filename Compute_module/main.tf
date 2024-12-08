743dd13ad1325c7e308e2fc4a0adaa949a76ad97
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 19:50:50 +0530

    Create main.tf

---
+++ Compute_module/main.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "public_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.pub_subnet_id
+  security_groups = [
+    var.pub_sg
+  ]
+
+  tags = {
+    Name = var.pub_instance_name
+  }
+
+}
+
+
+resource "aws_instance" "private_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.pvt_subnet_id
+  security_groups = [
+    var.pvt_sg
+  ]
+
+  tags = {
+    Name = var.pvt_instance_name
+  }
+
+}
