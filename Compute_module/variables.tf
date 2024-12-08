a28fd9aa3469c4227e6867bd5de5a83d5605e885
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 19:52:04 +0530

    Create variables.tf

---
+++ Compute_module/variables.tf
@@ -0,0 +1,34 @@
+variable "pub_instance_name" {
+  type    = string
+ 
+}
+variable "pvt_instance_name" {
+  type    = string
+ 
+}
+variable "ami_id" {
+  type    = string
+ 
+}
+variable "instance_type" {
+  type    = string
+ 
+}
+
+variable "pub_subnet_id" {
+  type    = string
+ 
+}
+variable "pub_sg" {
+  type    = string
+ 
+}
+
+variable "pvt_subnet_id" {
+  type    = string
+ 
+}
+variable "pvt_sg" {
+  type    = string
+ 
+}
