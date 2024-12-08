af9558e64e51cac7cdadc5a64684dd9c2d0fc5aa
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 19:54:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,3 +28,40 @@ variable "sg_port_pvt_01" {
   default     = 22
   description = "port"
 }
+
+
+
+variable "pub_instance_name" {
+  type    = string
+  default = "pub_in"
+}
+variable "pvt_instance_name" {
+  type    = string
+  default = "pvt_in"
+}
+variable "ami_id" {
+  type    = string
+  default = "ami-0e2c8caa4b6378d8c"
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+# variable "pub_subnet_id" {
+#   type = string
+#    
+# }
+# variable "pub_sg_id" {
+#   type = string
+#    
+# }
+
+# variable "pvt_subnet_id" {
+#   type = string
+#    
+# }
+# variable "pvt_sg_id" {
+#   type = string
+#   
+# }
