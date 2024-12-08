6488ee3cc4a760c546e651899d4672b42c5208a3
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 14:37:03 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = module.Networking_module.vpc_id
+}
+output "pub1_id" {
+  value = module.Networking_module.pub1_id
+}
+output "pub2_id" {
+  value = module.Networking_module.pub2_id
+}
+output "pvt1_id" {
+  value = module.Networking_module.pvt1_id
+}
+output "pvt2_id" {
+  value = module.Networking_module.pvt2_id
+}
