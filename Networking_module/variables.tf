4cbf8896b1699b2cf00dda62ccaeee0dee972e61
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 14:06:52 +0530

    Create variables.tf

---
+++ Networking_module/variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_cidr" {
+  type    = string
+ 
+}
+variable "pub1_cidr" {
+  type    = string
+ 
+}
+variable "pub2_cidr" {
+  type    = string
+  
+}
+variable "pvt1_cidr" {
+  type    = string
+
+}
+variable "pvt2_cidr" {
+  type    = string
+  
+}
+
