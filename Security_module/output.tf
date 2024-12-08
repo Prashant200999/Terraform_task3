acbfd049a93072346c8d917d3b8c46c69ed5276c
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2024-12-08 19:50:02 +0530

    Update output.tf

--- Security_module/output.tf
+++ Security_module/output.tf
@@ -1,8 +1,8 @@
-output "pub_sg_id" {
+output "pub_sg" {
   description = "ID of public sg"
   value = aws_security_group.Public_sg.id
 }
-output "pvt_sg_id" {
+output "pvt_sg" {
   description = "ID of public sg"
   value = aws_security_group.Private_sg.id
 }
